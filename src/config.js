d40d4bb3458d5b0c2ea91a8b95cf80dedebb5184
Kevin Damian Ta <dudeitsdamian@Kevins-MacBook.sanbrunocable.com>
2020-01-12 18:38:28 -0800

    Added api token validation

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://kevinakadamian19:Serendipity204!@localhost/unity',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://kevinakadamian19:Serendipity204!@localhost/unity-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://kevinakadamian19:Serendipity204!@localhost/unity',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://kevinakadamian19:Serendipity204!@localhost/unity-test',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'postgresql://kevinakadamian19:Serendipity204!@localhost/unity'
 }
